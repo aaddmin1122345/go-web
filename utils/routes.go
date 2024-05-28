13ebb92a0905a38546f43e5d16a1c7e942c67d5c
aaddmin1122345 <qxqzx@arch.localdomain>
2024-05-28 16:28:29 +0800

    重写了route的代码,让它更加简洁了

--- utils/routes.go
+++ utils/routes.go
@@ -5,25 +5,37 @@ import (
 	"net/http"
 )
 
-var UserApi = api.UserApiImpl{}
+//var UserApi = api.UserApiImpl{}
 
 type Route interface {
-	Handler()
+	Api()
+	Article()
+	Init()
 }
 
 type RouteImpl struct {
 }
 
-func (r RouteImpl) Handler() {
+func (r RouteImpl) Api() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/sayHello", api.SayHello)
 	http.HandleFunc("/api/login", UserApi.Login)
-	//http.HandleFunc("/api/GetUsersByStudID", UserApi.GetUsersByStudID)
-	http.HandleFunc("/api/getUserByUserName", UserApi.GetUserByUserName)
+	http.HandleFunc("/api/getUserByKeyword", UserApi.GetUserByKeyword)
 	http.HandleFunc("/api/updateUser", UserApi.UpdateUser)
 	http.HandleFunc("/api/register", UserApi.AddUser)
 	http.HandleFunc("/api/delUser", UserApi.DeleteUser)
 
+}
+
+func (r RouteImpl) Article() {
+	http.HandleFunc("/getArticleByKeyword", ArticleApi.GetArticleByKeyword)
+	//http.HandleFunc("/api/getArticleByKeyword", ArticleApi.GetArticleByKeyword)
+	http.HandleFunc("/api/createArticle", ArticleApi.CreateArticle)
+}
+
+func (r RouteImpl) Init() {
+	r.Api()
+	r.Article()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
